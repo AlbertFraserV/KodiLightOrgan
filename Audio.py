65a525bf12a9ef44ec943d43110516a4c003dc58
Zomby Fraser <agf4@njit.edu>
2023-01-29 18:40:55 -0500

    test

--- Audio.py
+++ Audio.py
@@ -3,7 +3,16 @@ import numpy as np
 
 class Audio:
     def __init__(self, audio_file):
-        self.time_series, self.sample_rate = librosa.load("Alone.mp3")
+        self.time_series, self.sample_rate = librosa.load(audio_file)
         self.stft = np.abs(librosa.stft(self.time_series, hop_length=512, n_fft=2048*4))
         self.spectrogram = librosa.amplitude_to_db(self.stft, ref=np.max)
         self.frequencies = librosa.core.fft_frequencies(n_fft=2048*4)  # getting an array of frequencies
+        self.times = librosa.core.frames_to_time(np.arange(self.spectrogram.shape[1]), sr=self.sample_rate, hop_length=512, n_fft=2048*4)
+        self.time_index_ratio = len(self.times)/self.times[len(self.times) - 1]
+        self.frequencies_index_ratio = len(self.frequencies)/self.frequencies[len(self.frequencies)-1]
+        self.min = self.spectrogram.min()
+
+    def get_dbs(self, target_time, freq):
+        return self.spectrogram[int(freq*self.frequencies_index_ratio)][int(target_time*self.time_index_ratio)]
+
+audio = Audio("Alone.mp3")
--- Visuals.py
+++ Visuals.py
@@ -1,9 +1,59 @@
 import pygame
+from pygame.locals import *
+from Audio import Audio
+import numpy as np
+import random
 
 class Visuals:
-    def __init__(self):
-        pass
+    def __init__(self, screen_width, screen_height):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.audio = Audio("Alone.mp3")
+        self.max_db = self.audio.spectrogram.min()
 
     def play(self):
         pygame.init()
-        screen = pygame.display.set_mode((640, 240))
+        running = True
+        t = pygame.time.get_ticks()
+        getTicksLastFrame = t
+        pygame.mixer.music.load("Alone.mp3")
+        pygame.mixer.music.play(0)
+        screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        freq_ranges = np.arange(100, int(self.audio.frequencies.max()), len(self.audio.frequencies))
+        while running:
+            screen.fill((0,0,0))
+            pygame.display.flip()
+            t = pygame.time.get_ticks()
+            deltaTime = (t - getTicksLastFrame) / 1000.0
+            getTicksLastFrame = t
+            rects = []
+            x = 0; y = 0
+            for f in freq_ranges:
+                db = self.audio.get_dbs(pygame.mixer.music.get_pos()/1000.0, self.audio.frequencies[freq_ranges[f]])
+                self.draw_rect(screen, (random.int(0,254),random.int(0,254),random.int(0,254)), (10,10), (x,y), self.calc_alpha(self.max_db, db))
+                x += 10; y += 10
+                rects.append(db)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            pygame.display.flip()
+            # for freq in self.audio.frequencies
+        pygame.quit()
+
+    def draw_rect(self, screen, color, size, location, alpha):
+        # rect = Rect(location[0],location[1],size[0],size[1])
+        # rect.center = ((location[0], location[1]))
+        s = pygame.Surface(size, pygame.SRCALPHA)  
+        s.fill((color[0],color[1],color[2],round(alpha))) 
+        screen.blit(s, location)
+        # pygame.display.update()    
+        # pygame.draw.rect(screen, color, rect)
+        pygame.display.flip()
+
+    def calc_alpha(self, max_db, db):
+        return abs(1-(db/max_db))*255
+
+vis = Visuals(1000,1000)
+vis.play()
